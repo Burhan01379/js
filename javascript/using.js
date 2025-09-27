dfcbd4443a8417ecb66e6641291352e8e6b3f79a
Burhan Dar <burhandar096@gmail.com>
2025-09-27 12:15:14 +0530

    Learned Datatypes

---
+++ javascript/using.js
@@ -0,0 +1,12 @@
+const accountId = "144553"
+let accountEmail ="burhandar096@gmail.com"
+var accountPassword ="12345"
+accountCity  ="kashmir"
+
+//accountId =2 //not allowed
+accountEmail ="buru@gmail.com"
+accountPassword ="212121"
+accountCity ="srinagar"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountId])
