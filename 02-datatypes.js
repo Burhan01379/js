2317506ebab1f90c0e841371c6433fb32ecedb2d
Burhan dar <burhandar096@gmail.com>
2025-10-02 11:25:20 +0530

    Add files via upload

---
+++ 02-datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3)//we are using nodejs ,note browser
+
+console.log(3
+    +3)//code readability should be high
+
+console.log("burhan")
+
+
+let name ="burhan"//string datatype
+let age = 18//number datatype=>2 to power 53 .for large number we use bigint
+let isloggedIn =false//boolean =>true/fasle
+let stste=null
+//null=>standalone value
+//undefined=>
+    //symbol to find uniqueness 
+//object
+console.log(typeof name);
+
+
+
+
