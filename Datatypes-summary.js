41451ad4024750a47f4b8ce8c3f1a015cc8e91f9
Burhan dar <burhandar096@gmail.com>
2025-10-21 19:34:18 +0530

    Add files via upload

---
+++ Datatypes-summary.js
@@ -0,0 +1,39 @@
+// primitive
+
+// 7 types : string, number, boolearn, null, undefined, symbol, BigInt
+
+//const score = 100
+//const scoreValue =100.3
+
+//const isLoggedIn = false
+//const outsideTemp = null
+//let userEmail;
+
+
+//const id = Symbol('123')
+//const anotherId =symbol('123')
+
+//console.log(Id ===anotherId);
+
+//const bigNumber = 21124123n
+
+
+
+//reference (non primitive)
+
+// array, objects, functions.
+
+const heros = ["tufail", "reyaz","burhan",]
+let myObj{
+
+    name:"basit"
+    age: 24,
+}
+
+
+const myFunction=function(){
+    console.log("hello word");
+
+}
+
+ console.log(typesof heros);
---
+++ comparision.js
@@ -0,0 +1,14 @@
+//console.log("2" > 1);
+//console.log(2 != 1);
+
+console.log( null >0);
+console.log( null ==0);
+console.log( null >=0);
+
+
+console.log(undefined == 0);
+//these type of comparision has to be avoid
+
+// ===
+
+console.log("2" == 2);
