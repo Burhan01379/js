5cf2c9d708619a47627fab40e63d52ec8be26e7d
Burhan dar <burhandar096@gmail.com>
2025-10-27 10:26:32 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,36 @@
+//const name ="burhan"
+//const repoCount =50
+
+//console.log(`hello my name is ${name} and my repo count is${repoCount}`);
+
+
+const gameName = new String ('burhan-ab')
+
+//console.log (gameName[0]);
+//console.log(gameName.__proto__);
+
+
+//console.log(gameName.length);
+//console.log (gameName.toUpperCase());
+//console.log (gameName.charAT(2));
+//console.log (gameName.indexOf('h'));
+
+//const newString = gameName.substring(0,4)
+//console.log (newString);
+
+//connst anotherString = gameName.slice(-8,4)
+//console.log (anotherString);
+
+//const newStringOne ="  burhan   "
+//console.log(newStringOne);
+//console.log(newStringOne.trim());
+
+const url ="https://burhan.com/burhan%20dar"
+
+console.log(url.replace('%20','-'))
+
+
+console.log( url.includes('sunday'))
+
+
+console.log (gameName.split('-'));
---
+++ using.js
@@ -0,0 +1,18 @@
+const accountId = "144553"
+let accountEmail ="burhandar096@gmail.com"
+var accountPassword ="12345"
+accountCity  ="kashmir"
+
+//accountId =2 //not allowed
+accountEmail ="buru@gmail.com"
+accountPassword ="212121"
+accountCity ="srinagar"
+
+console.log(accountId);
+/*
+prefer  not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountId]);
+
